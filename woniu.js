b4180d2dbdcb9a60c24af2a2bcf9f5adf1771fa7
chindden <34678156+chindden@users.noreply.github.com>
2021-09-18 14:33:28 +0800

    Update woniu.js

--- woniu.js
+++ woniu.js
@@ -1,5 +1,13 @@
+/*
+网易蜗牛读书 解锁特权
+原作者: yxiaocai & JO2EY
+
+***************************
+QuantumultX:
+
 [rewrite_local]
-^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json url script-response-body https://raw.githubusercontent.com/chindden/best/main/woniu.js
+^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wnyd.js
+
 [mitm]
 hostname = p.du.163.com
 
@@ -7,7 +15,7 @@ hostname = p.du.163.com
 Surge4 or Loon:
 
 [Script]
-http-response ^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/chindden/best/main/woniu.js
+http-response ^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wnyd.js
 
 [MITM]
 hostname = p.du.163.com
