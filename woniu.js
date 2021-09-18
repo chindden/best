d6df09d3b98a321721e4439e9e57817c61a2c9a8
chindden <34678156+chindden@users.noreply.github.com>
2021-09-18 14:26:34 +0800

    Update and rename woniu to woniu.js

---
+++ woniu.js
@@ -0,0 +1,21 @@
+[rewrite_local]
+^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json url script-response-body https://raw.githubusercontent.com/chindden/best/main/woniu.js
+[mitm]
+hostname = p.du.163.com
+
+***************************
+Surge4 or Loon:
+
+[Script]
+http-response ^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/chindden/best/main/woniu.js
+
+[MITM]
+hostname = p.du.163.com
+
+**************************/
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj.tradeEndTime = 1837538090000;
+body = JSON.stringify(obj);
+$done({body});
