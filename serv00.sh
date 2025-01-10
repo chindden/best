1bf620ec708155020db72f3cc6b4ee6baeeff36f
chindden <34678156+chindden@users.noreply.github.com>
2025-01-10 22:23:26 +0800

    Update serv00.sh

--- serv00.sh
+++ serv00.sh
@@ -34,7 +34,7 @@ green "你选择的IP为: $IP"
 read_uuid() {
         reading "请输入统一的uuid密码 (建议回车默认随机): " UUID
         if [[ -z "$UUID" ]]; then
-	   UUID="chindden"
+	   UUID="oyrobin"
         fi
 	green "你的uuid为: $UUID"
 }
@@ -169,8 +169,8 @@ argo_configure() {
     fi
     if [[ "$argo_choice" == "g" || "$argo_choice" == "G" ]]; then
         ARGO_DOMAIN="gou.tida.us.kg"
-	reading "请输入argo固定隧道域名: " ARGO_DOMAIN
-        green "你的argo固定隧道域名为: $ARGO_DOMAIN"
+	reading "请输入argo固定隧道域名: $ARGO_DOMAIN"
+        green "你的argo固定隧道域名为:$ARGO_DOMAIN"
         reading "请输入argo固定隧道密钥（Json或Token。当你粘贴Token时，必须以ey开头）: " ARGO_AUTH
         green "你的argo固定隧道密钥为: $ARGO_AUTH"
     else
